2d88feb8bcfa81dcdad85365b74938252926cfea
Edder Luan <eluann100@gmail.com>
2022-04-16 18:01:33 -0300

    Initial commit

---
+++ backend/src/main/java/com/eluanps/sistemacontrole/SistemacontroleApplication.java
@@ -0,0 +1,13 @@
+package com.eluanps.sistemacontrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemacontroleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemacontroleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eluanps/sistemacontrole/SistemacontroleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eluanps.sistemacontrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemacontroleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
