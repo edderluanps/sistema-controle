89e2cf9d7a95680eb64d786a9fda3fff38130135
Edder Luan <eluann100@gmail.com>
2022-04-30 22:57:39 -0300

    Editando dados de clientes vindos da Api

--- frontend/src/app/clientes.service.ts
+++ frontend/src/app/clientes.service.ts
@@ -15,8 +15,17 @@ export class ClientesService {
    salvar(cliente : Cliente) : Observable<Cliente>{
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
   }
+
+  atualizar(cliente : Cliente) : Observable<any>{
+    return this.http.put<Cliente>(`http://localhost:8080/api/clientes/${cliente.id}`, cliente);
+  }
+
   getClientes() : Observable<Cliente[]>{
     return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
+  }
+
+  getClientesById(id: number) : Observable<Cliente>{
+    return this.http.get<any>(`http://localhost:8080/api/clientes/${id}`);
   }
 
 }
